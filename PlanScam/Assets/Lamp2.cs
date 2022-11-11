0d07d5fc09c575535e5002863f7219526b8012a0
sadnessakura <djmax260801@mail.ru>
2022-11-11 21:36:26 +0300

    New
    Syda

---
+++ PlanScam/Assets/Lamp2.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp2 : MonoBehaviour
+{
+
+    public int Number;
+    public int Num;
+    public Light Lamp;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Number = Random.Range(0, 30);
+        if (Number < Num)
+        {
+            Lamp.intensity = 0;
+        }
+        else
+        {
+            Lamp.intensity = 1;
+        }
+    }
+
+}
